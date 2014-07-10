1a07335b741ee1e1185f5aba4b5452bd8c1acf5b
Remy <remy.maucourt@yahoo.fr>
2014-07-10 20:22:32 +0200

    omniauth fonctionnel

--- Gemfile
+++ Gemfile
@@ -3,4 +3,6 @@ source "https://rubygems.org"
 
 gem "sinatra"
 gem "shotgun"
-gem "oauth2"
+gem 'pry'
+gem "omniauth"
+gem "omniauth-google-oauth2"
--- app.rb
+++ app.rb
@@ -1,14 +1,41 @@
+require 'pry'
 require 'sinatra'
-require 'oauth2'
+require 'omniauth'
+require 'omniauth-google-oauth2'
+
+enable :sessions
+
+configure do
+	use Rack::Session::Cookie, :secret => Digest::SHA1.hexdigest(rand.to_s)
+	KEY='306691358040-tj1tltsv8ttd3gtmd5e11l42bej1fn2t.apps.googleusercontent.com'
+	SECRET='fm2nmcEkUEMeLYllE1pE6NKn'
+end
+
+use OmniAuth::Builder do
+  provider :google_oauth2, KEY, SECRET
+end
+
+def access_token
+  consumer = OAuth::Consumer.new(KEY, SECRET)
+  OAuth::AccessToken.from_hash(consumer, session[:oauth_credentials])
+end
 
 get '/' do 
 	erb :auth
+end
+
+get '/auth/google_oauth2/callback' do
+  halt(401, 'Non Autorisé') unless env['omniauth.auth']
+  session[:oauth_credentials] = env['omniauth.auth']['credentials']
+  session[:oauth_info] = env['omniauth.auth']['info']
+  session[:authenticated] = true
+  redirect url('/')
 end
 
 get '/presentation' do
 	"Hackathon Simplon YRNOW"
 end
 
-get '/hi' do
+get '/home' do
 	erb :home
 end
